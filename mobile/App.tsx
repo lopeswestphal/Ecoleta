b7e6a8f3bba4cfd7595f4965efaabc41e1327e32
lopeswestphal <lopeswestphal@hotmail.com>
2020-06-08 09:23:30 -0300

    Pagina de Home do mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { AppLoading } from 'expo';
+
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+
+import Home from './src/pages/Home'
+
+/**
+ * <> </> -> tag chamada de fregment onde nao tem nenhum conteudo, porque nao podemos retornar dois elementos
+ * Sem estarem em um "tipo de container" ou melhor so retornar um elemento.
+*/
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  });
+
+  if (!fontsLoaded ){
+    return <AppLoading />
+  }
+
+  return (
+    <>
+      <StatusBar 
+        barStyle="dark-content" 
+        //backgroundColor="transparent" 
+        translucent
+      />
+      <Home />
+    </>
+  );
+}
